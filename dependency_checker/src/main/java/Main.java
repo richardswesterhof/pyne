47be541e9fa9dcccb3a8e0c6b3d1165a2cdac7cb
Richard Westerhof <38462668+richardswesterhof@users.noreply.github.com>
2020-12-27 19:22:26 +0100

    did the stuff we talked about:
    -use classes instead of packages: done
    -check source code: impossible
    -skip classes without dependencies: they are not listed in the dependencies but they are listed in the classes (seemed like a good compromise)
    -also list subclasses: done, with the usual syntax (class$subclass)
    
    Furthermore, I added Apache commons cli.
    And there is now the option to pretty print the file (which includes extensive dependency listings including name and whether or not packages are internal), or not pretty print, to keep file size low (a fully detailed file can be >25mb :))

--- dependency_checker/src/main/java/Main.java
+++ dependency_checker/src/main/java/Main.java
@@ -1,5 +1,6 @@
 import functionality.Comparator;
 import functionality.XMLHandler;
+import org.apache.commons.cli.*;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
@@ -12,17 +13,28 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
-        if(args.length < 2) {
-            System.err.println("Must specify the paths of the Structure101 csv file and the Pyne graphml file");
-            System.err.println("Example: java -jar dependency_checker.jar " +
-                    "\"../graph-files/tajo_dependencies_by_structure101.csv\" " +
-                    "\"../graph-files/tajo_dependencies_by_pyne.graphml\" ");
-            System.exit(1);
-        }
-        String structure101Path = args[0];
-        String pynePath = args[1];
+        Options options = new Options();
+        options.addOption("s", "structure101-file", true, "The file to use for Structure101's analysis");
+        options.addOption("p", "pyne-file", true, "The file to use for Pyne's analysis");
+        options.addOption("hr", "human-readable", false, "If specified, the file will be formatted to be easier to read, this will lead to (much) bigger file sizes however");
+        options.addOption("indent", false, "If specified, the file will be indented properly to be easier to read. The human-readable option implies this already");
+
+
+        String structure101Path;
+        String pynePath;
 
         try {
+            CommandLineParser parser = new DefaultParser();
+            CommandLine cmd = parser.parse(options, args);
+
+            structure101Path = cmd.getOptionValue("s");
+            pynePath = cmd.getOptionValue("p");
+
+            if(structure101Path == null || pynePath == null) {
+                printHelp(options);
+                System.exit(1);
+            }
+
             File structure101Matrix = new File(structure101Path);
             File pyneGraph = new File(pynePath);
             // TODO: make this a parameter in CLI
@@ -32,19 +44,19 @@ public class Main {
 
             // initialize comparator
             System.out.println("Initializing comparator");
-            Comparator comparator = new Comparator(structure101Matrix, pyneGraph).importFileData();
+            Comparator comparator = new Comparator(structure101Matrix, pyneGraph, cmd).importFileData();
 
-            // collect packages
-			System.out.println("Collecting packages");
-            comparator.collectAllPackages();
+            // collect classes
+			System.out.println("Collecting classes");
+            comparator.collectAllClasses();
 
             // compare packages
 			System.out.println("Comparing packages");
-            Document doc = comparator.compareResults();
+            Document doc = comparator.compareResults(cmd.hasOption("hr"));
 
             // output differences to xml file
             System.out.println("Writing to output file");
-            XMLHandler.writeXML(doc, output);
+            XMLHandler.writeXML(doc, output, cmd.hasOption("hr") || cmd.hasOption("indent"));
 
             // live happily ever after :)
             System.out.println("Done!");
@@ -60,6 +72,14 @@ public class Main {
         } catch(TransformerException e) {
             System.err.println("Something went wrong when writing output");
             e.printStackTrace();
+        } catch (ParseException e) {
+            System.err.println("Something went wrong when parsing cli arguments");
+            e.printStackTrace();
         }
+    }
+
+    private static void printHelp(Options options) {
+        HelpFormatter hf = new HelpFormatter();
+        hf.printHelp("java -jar dependency_checker.jar", options);
     }
 }
